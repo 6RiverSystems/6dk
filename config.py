71c7accaa6cd252512d266a3b38e874fb73efb49
Jamie <j.rolan.hall@gmail.com>
2019-01-19 11:47:23 -0500

    fix: improved feed filtering experience

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     ADMIN_TOKENS = ['09dfe030-e97d-4c59-b440-c936d212e0ab']
     DEV_FS_BASE_URL = 'https://devkit.6river.org'
     MAX_PROFILE_COUNT = 5
-    ELEMENTS_PER_PAGE = 50
+    ELEMENTS_PER_PAGE = 100
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
