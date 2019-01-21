90808c481f39aebd756f936c42c4ed1a63200930
Joseph Hughes <jjhughes57@gmail.com>
2019-01-21 16:57:47 -0500

    fix: entrypoint

--- docker-deps/entrypoint.sh
+++ docker-deps/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+cd /app || exit
 
-flask db upgrade
+while true; do
+    pipenv run flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
+
 
 pipenv run gunicorn --bind unix:/app/6dk.sock -m 777 wsgi --workers=2 &
 
