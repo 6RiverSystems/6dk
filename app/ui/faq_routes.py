b65d4ab76ce83b2ac9dd7fd15e08233c304c6379
Jamie <j.rolan.hall@gmail.com>
2019-03-30 13:46:00 -0400

    fix: change buttons to 6rweb style, dynamic FAQ page

--- app/ui/faq_routes.py
+++ app/ui/faq_routes.py
@@ -1,15 +1,34 @@
 import json
 
-from flask import render_template
+from flask import render_template, request, url_for, jsonify
 from flask_login import login_required
 
 from app import app
+from app.plugins.faq_helper import get_faqs
 
 
-@app.route('/faq', methods=['GET'])
+@app.route('/faq', methods=['GET', 'POST'])
 @login_required
 def faq_main():
-    with open('app/templates/json/faqs.json', 'r') as f:
-        faqs = json.loads(f.read())
-    faqs = sorted(faqs, key=lambda k: k['Q'])
-    return render_template('faq.html', faqs=faqs)
+    query = request.args.get('q')
+    faqs = get_faqs(request.method)
+    if query:
+        faqs = [faq for faq in faqs if query.lower() in str(faq).lower()]
+
+    options = [
+        {
+            'linkname': faq['Q'],
+            'meta_html': faq['A'],
+        } for faq in faqs
+    ]
+
+    option_block = render_template('option_block.html',
+                                   options=options, query=query,
+                                   bold_text=True)
+    if request.method == 'GET':
+        return render_template(
+            'options.html',
+            option_block=option_block,
+            search=url_for('faq_main'))
+    else:
+        return jsonify({'html': option_block})
